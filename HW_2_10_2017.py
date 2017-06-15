9dc6a128c1517e7680c9f305cc15d9f02244b320
chenikita <che@rt.ru>
2017-06-15 22:26:28 +0300

    HW_1_2
    Первое и второе домашние задания второго захода

--- HW_2_10_2017.py
+++
@@ -1,9 +0,0 @@
-# Создать список с тремя значениями 'to-delete' и нескольми любыми другими, удалить из него 
-# все значения 'to-delete'
-
-to_del  = ['to-delete', 123, str, float, 'to-delete', 'smth', [1,2,3,4,5], 'to-delete']
-
-for i in to_del:
-	if i == 'to-delete':
-		to_del.remove(i)
-print(to_del)
--- HW_2_6_2017.py
+++
@@ -1,8 +0,0 @@
-# Пройти по всем числам от 1 до 100, написать в консоль, какие из них делятся на 7, 
-# а какие - нет
-
-for i in range(1, 100):
-	if i % 7 == 0:
-		print('and {} is!'.format(i))
-	else:
-		print('{} is not multiple of 7'.format(i))
